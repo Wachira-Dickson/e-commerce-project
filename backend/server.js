52469bff5496e17de7519e1e33840e72e178ef23
Your Name <dicksonwachira886@gmail.com>
2026-04-11 19:20:43 +0300

    Add the product model, controllers, routes and the protectRoute

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from 'cookie-parser';
 
-import authRoutes from "./routes/auth.route.js"
+import authRoutes from "./routes/auth.route.js";
+import productRoutes from "./routes/product.route.js";
 
 import { connectDB } from "./lib/db.js";
 
@@ -10,7 +12,12 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json());        //allows you to parse the body of the request
+app.use(cookieParser());       //allows you to parse the cookies of the request
+
+
 app.use("/api/auth", authRoutes)
+app.use("/api/products", productRoutes)
 
 app.listen(PORT, () => {
     console.log("Server is running on port http://localhost:" + PORT);
