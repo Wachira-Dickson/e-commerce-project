be42161f7d242531df96316ae7dbd3e34b86c298
Your Name <dicksonwachira886@gmail.com>
2026-04-26 14:14:57 +0300

    Add payment routes and controllers

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,8 @@ import cookieParser from 'cookie-parser';
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
+import couponRoutes from "./routes/coupon.route.js";
+import paymentRoutes from "./routes/payment.route.js";
 
 import { connectDB } from "./lib/db.js";
 
@@ -17,9 +19,11 @@ app.use(express.json());        //allows you to parse the body of the request
 app.use(cookieParser());       //allows you to parse the cookies of the request
 
 
-app.use("/api/auth", authRoutes)
-app.use("/api/products", productRoutes)
-app.use("/api/cart", cartRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
+app.use("/api/coupons", couponRoutes);
+app.use("/api/payments", paymentRoutes);
 
 app.listen(PORT, () => {
     console.log("Server is running on port http://localhost:" + PORT);
